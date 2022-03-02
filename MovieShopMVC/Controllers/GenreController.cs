1e997ad0ddeedcafd234f9baf4f82e544845ce93
JW <JW@DESKTOP-VSBVS6P>
2022-03-01 17:35:37 -0800

    Add project files.

---
+++ MovieShopMVC/Controllers/GenreController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApplicationCore.Models;
+using ApplicationCore.Contracts.Services;
+namespace MovieShopMVC.Controllers
+{
+    public class GenreController : Controller
+    {
+        private readonly IGenreService _genreService;
+        public GenreController(IGenreService genreService)
+        {
+            _genreService = genreService;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.CompanyName = "Antra";
+            ViewData["CurrentDate"] = DateTime.Now;
+            var data = _genreService.GetAllGenres();
+            return View(data);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(GenreModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _genreService.InsertGenre(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _genreService.DeleteGenre(id);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ MovieShopMVC/Controllers/MovieController.cs
@@ -0,0 +1,47 @@
+﻿using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Models;
+namespace MovieShopMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieService _movieService;
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        public IActionResult Index()
+        {
+            var movies = _movieService.GetMoviesByPagination(10, 1, "");
+            return View(movies);
+        }
+        [HttpGet]
+        public IActionResult Genre(int id)
+        { 
+       var data =  _movieService.MoviesSameGenre(id);
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(MovieCreateRequestModel model)
+        {
+            try
+            {
+                _movieService.AddMovie(model);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(model);
+            }
+        }
+
+       
+    }
+}
